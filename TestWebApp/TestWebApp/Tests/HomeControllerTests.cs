169d12ab8f150cba45a025580cca463ad23dab35
Aravinda Wijesekara <aravinda.wijesekara@nagarro.com>
2023-09-18 12:04:31 +0530

    Test Web App

---
+++ TestWebApp/TestWebApp/Tests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using TestWebApp.Controllers; // Update with your actual namespace
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace TestWebApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
